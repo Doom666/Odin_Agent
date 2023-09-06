38b5d8f76ad61b35d5e05d01c55f8798710c9514
CarlosMK <kaahrl@gmail.com>
2023-09-06 07:53:38 +0200

    testing reasoning engines

--- tests/VertexAItest.py
+++ tests/VertexAItest.py
@@ -1,14 +1,23 @@
-from langchain.chat_models import ChatVertexAI
-from langchain.prompts.chat import (
-    ChatPromptTemplate,
-    SystemMessagePromptTemplate,
-    HumanMessagePromptTemplate,
-)
-from langchain.schema import HumanMessage, SystemMessage
+from langchain.llms import VertexAI
+from langchain.chains import LLMChain
+from langchain.prompts import PromptTemplate
 
-chat = ChatVertexAI()
+import time
+llm = VertexAI(project="white-sign-398014")
 
 
-chain = LLMChain(llm=chat, prompt=chat_prompt)
+template = """Please, explain me what {subject} is as if I was really really stupid."""
+prompt = PromptTemplate(template=template, input_variables=["subject"])
+chain = LLMChain(llm=llm, prompt=prompt)
 
-chain.run(input_language="English", output_language="French", text="I love programming.")
+
+while True:
+    respponse = chain.predict(subject=input("What do you want to learn from me, inferior carbon object?"))
+    time.sleep(3)
+    print(respponse)
+    time.sleep(3)
+    template2 = """Please, explain in a ridiculously simpler manner this text: {subject} """
+    prompt2 = PromptTemplate(template=template2, input_variables=["subject"])
+    chain2 = LLMChain(llm=llm, prompt=prompt2)
+    chain2.predict(subject=respponse)
+    time.sleep(3)
