071ccb6c5a39bb42fcccc80774e72bfca9484238
Doom666 <kaahrl@gmail.com>
2023-09-08 09:15:32 +0200

    No Idea of whats gitting on

--- venv/Lib/site-packages/langchain/chat_loaders/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Load chat messages from common communications platforms for finetuning.
-
-This module provides functions to load chat messages from various
-communications platforms such as Facebook Messenger, Telegram, and
-WhatsApp. The loaded chat messages can be used for finetuning models.
-"""
--- venv/Lib/site-packages/langchain/vectorstores/redis/__init__.py
+++
@@ -1,9 +0,0 @@
-from .base import Redis
-from .filters import (
-    RedisFilter,
-    RedisNum,
-    RedisTag,
-    RedisText,
-)
-
-__all__ = ["Redis", "RedisFilter", "RedisTag", "RedisText", "RedisNum"]
